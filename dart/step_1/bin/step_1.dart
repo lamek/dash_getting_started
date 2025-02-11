5e00eacde30e778ee152faac349a4547c6877122
Eric Windmill <eric@ericwindmill.com>
2025-02-11 14:15:40 -0500

    initial

---
+++ dart/step_1/bin/step_1.dart
@@ -0,0 +1,5 @@
+import 'package:step_1/step_1.dart' as step_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${step_1.calculate()}!');
+}
---
+++ dart/step_1/test/step_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:step_1/step_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
